8823ec693e8ffa04e873cd544aab292d91e1a5c2
Rifat <rifat.aiub@outlook.com>
2018-09-17 15:51:02 +0600

    check

--- resources/views/Admin/Others/others_list.blade.php
+++ resources/views/Admin/Others/others_list.blade.php
@@ -66,17 +66,6 @@
         </div>
     </div>
     <div class="wrapper wrapper-content animated fadeInRight ecommerce">
-        <div class="row">
-            <div class="col-lg-12">
-                @if (session('status'))
-
-                    <div class="alert alert-success">
-                        <strong>{{ session('status') }}</strong>
-                    </div>
-
-                @endif
-            </div>
-        </div>
         
         @include('Admin.Layouts.message')
 
@@ -84,10 +73,8 @@
             <div class="col-lg-12">
                 <div class="tabs-container">
                     <ul class="nav nav-tabs">
-                        <li class="active"><a data-toggle="tab" href="#tab-2"> Store</a></li>
-                        <li class=""><a data-toggle="tab" href="#tab-3"> Social</a></li>
-                        <li class=""><a data-toggle="tab" href="#tab-4"> Local</a></li>
-                        <li class=""><a data-toggle="tab" href="#tab-5"> Images</a></li>
+                        <li class="active"><a data-toggle="tab" href="#tab-1"> Store</a></li>
+                        <li class=""><a data-toggle="tab" href="#tab-2"> Social</a></li>
                     </ul>
                     <form method="POST" action="{{ url('/vendor/general_settings/update_general_settings_request') }}" class="form-horizontal" id="form-generalSettings" enctype="multipart/form-data">
                     {{ csrf_field() }}
@@ -95,7 +82,7 @@
                     <input type="hidden" id="id" name="id" value="{{ $get_general_settings[0]->id }}" class="form-control">
 
                     <div class="tab-content">
-                        <div id="tab-2" class="tab-pane active">
+                        <div id="tab-1" class="tab-pane active">
                             <div class="panel-body">
 
                                 <fieldset class="form-horizontal">
@@ -113,53 +100,11 @@
                                             <span class="help-block m-b-none"></span>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label">Company Owner: <font color="red">*</font></label>
-                                        <div class="col-sm-10">
-                                            <input name="owner" class="form-control" type="text" value="{{ $get_general_settings[0]->owner }}" placeholder="Enter Company Owner">
-                                            <span class="help-block m-b-none"></span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label">Address: <font color="red">*</font></label>
-                                        <div class="col-sm-10">
-                                            <input name="address" class="form-control" type="text" value="{{ $get_general_settings[0]->address }}" placeholder="Enter Address">
-                                            <span class="help-block m-b-none"></span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label">Geocode</label>
-                                        <div class="col-sm-10">
-                                            <input name="geocode" class="form-control" type="text" value="{{ $get_general_settings[0]->geocode }}" placeholder="Enter Geocode">
-                                            <span class="help-block m-b-none"></span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label">Email: <font color="red">*</font></label>
-                                        <div class="col-sm-10">
-                                            <input name="email" class="form-control" type="text" value="{{ $get_general_settings[0]->email }}" placeholder="Enter Email">
-                                            <span class="help-block m-b-none"></span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label">Telephone: <font color="red">*</font></label>
-                                        <div class="col-sm-10">
-                                            <input name="telephone" class="form-control" type="text" value="{{ $get_general_settings[0]->telephone }}" placeholder="Enter Telephone">
-                                            <span class="help-block m-b-none"></span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label">Copyright: <font color="red">*</font></label>
-                                        <div class="col-sm-10">
-                                            <input name="copyright" class="form-control" type="text" value="{{ $get_general_settings[0]->copyright }}" placeholder="Enter Copyright">
-                                            <span class="help-block m-b-none"></span>
-                                        </div> 
-                                    </div>
                                 </fieldset>
 
                             </div>
                         </div>
-                        <div id="tab-3" class="tab-pane">
+                        <div id="tab-2" class="tab-pane">
                             <div class="panel-body">
 
                                 <fieldset class="form-horizontal">
